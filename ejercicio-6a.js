4daa124ef642e3a9886acab8b7462d6f0c28f727
CCOviedo <37302280+CCOviedo@users.noreply.github.com>
2019-10-21 16:10:57 +0200

    Add files via upload

---
+++ ejercicio-6a.js
@@ -0,0 +1,5 @@
+var nombreCompleto = "Carlos Crespo Oviedo";
+var edad = 22;
+var diasSemana = ['Lunes',"Martes", 'Miercoles',"Jueves","Viernes", 'Sabado', 'Domingo'];
+var valGravedad = 9.8;
+var tresImpares = [3,5,7]
